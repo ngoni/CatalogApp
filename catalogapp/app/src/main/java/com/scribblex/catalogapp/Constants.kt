393cfd1e79c497b19ba7b46f554b8587fd923e8d
Ngoni Munyaradzi <ngoni.munyaradzi@rapiddeploy.com>
2022-01-13 17:29:09 +0200

    Add support for multiple view holders

--- catalogapp/app/src/main/java/com/scribblex/catalogapp/Constants.kt
+++ catalogapp/app/src/main/java/com/scribblex/catalogapp/Constants.kt
@@ -2,4 +2,13 @@ package com.scribblex.catalogapp
 
 object Constants {
     const val BASE_URL: String = "http://mobcategories.s3-website-eu-west-1.amazonaws.com"
+
+    // supported app view types for Category listing screen
+    const val VIEW_TYPE_UNSUPPORTED: Int = 0
+    const val VIEW_TYPE_HEADER: Int = 1
+    const val VIEW_TYPE_LIST_ITEM: Int = 2
+
+    // Hard-coded values from the API
+    const val CATEGORY_FOOD_ID: Int = 36802
+    const val CATEGORY_DRINK_ID: Int = 36803
 }
--- catalogapp/app/src/main/java/com/scribblex/catalogapp/ui/CatalogListAdapter.kt
+++ catalogapp/app/src/main/java/com/scribblex/catalogapp/ui/CatalogListAdapter.kt
@@ -1,6 +1,5 @@
 package com.scribblex.catalogapp.ui
 
-import com.scribblex.catalogapp.data.entities.Products
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.view.LayoutInflater
@@ -11,48 +10,79 @@ import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.scribblex.catalogapp.Constants
+import com.scribblex.catalogapp.Constants.VIEW_TYPE_HEADER
+import com.scribblex.catalogapp.Constants.VIEW_TYPE_LIST_ITEM
+import com.scribblex.catalogapp.Constants.VIEW_TYPE_UNSUPPORTED
+import com.scribblex.catalogapp.data.entities.Products
 import com.scribblex.catalogapp.databinding.FragmentCatalogItemBinding
+import com.scribblex.catalogapp.databinding.FragmentCatalogItemHeaderBinding
 
 class CatalogListAdapter(
     private val callback: (Products) -> Unit,
-) : RecyclerView.Adapter<CatalogListAdapter.ViewHolder>() {
+) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
     private val values: MutableList<Products> = mutableListOf()
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        return ViewHolder(
-            FragmentCatalogItemBinding.inflate(
-                LayoutInflater.from(parent.context),
-                parent,
-                false
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return when (viewType) {
+            VIEW_TYPE_HEADER -> HeaderViewHolder(
+                FragmentCatalogItemHeaderBinding.inflate(
+                    LayoutInflater.from(parent.context),
+                    parent,
+                    false
+                )
             )
-        )
+            VIEW_TYPE_LIST_ITEM -> CatalogItemViewHolder(
+                FragmentCatalogItemBinding.inflate(
+                    LayoutInflater.from(parent.context),
+                    parent,
+                    false
+                )
+            )
+            else -> {
+                throw IllegalArgumentException()
+            }
+        }
     }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+    override fun onBindViewHolder(viewholder: RecyclerView.ViewHolder, position: Int) {
         val item = values[position]
-        holder.apply {
-            productTitle.text = item.name 
+        when (viewholder) {
+            is HeaderViewHolder -> {
+                viewholder.headerName.text = item.name
+            }
+            is CatalogItemViewHolder -> {
+                viewholder.apply {
 
-            val url = Constants.BASE_URL + item.url
-            Glide.with(imageView.context)
-                .load(url)
-                .placeholder(ColorDrawable(Color.GRAY))
-                .centerCrop()
-                .circleCrop()
-                .into(imageView)
-            productItemContainer.setOnClickListener { callback.invoke(item) }
+                    productTitle.text = item.name
+
+                    val url = Constants.BASE_URL + item.url
+                    Glide.with(imageView.context)
+                        .load(url)
+                        .placeholder(ColorDrawable(Color.GRAY))
+                        .centerCrop()
+                        .circleCrop()
+                        .into(imageView)
+
+                    productItemContainer.setOnClickListener { callback.invoke(item) }
+                }
+            }
+        }
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return when (values[position].categoryId) {
+            VIEW_TYPE_HEADER -> {
+                VIEW_TYPE_HEADER
+            }
+            VIEW_TYPE_LIST_ITEM -> {
+                VIEW_TYPE_LIST_ITEM
+            }
+            else -> VIEW_TYPE_UNSUPPORTED
         }
     }
 
     override fun getItemCount(): Int = values.size
-
-    inner class ViewHolder(binding: FragmentCatalogItemBinding) :
-        RecyclerView.ViewHolder(binding.root) {
-        val imageView: AppCompatImageView = binding.productImage
-        val productTitle: AppCompatTextView = binding.productName
-        val productItemContainer: ConstraintLayout = binding.productItemContainer
-    }
 
     fun updateData(items: List<Products>?) {
         items?.let {
@@ -60,6 +90,18 @@ class CatalogListAdapter(
             values.addAll(items)
             notifyItemRangeChanged(0, items.size)
         }
+    }
+
+    inner class CatalogItemViewHolder(binding: FragmentCatalogItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        val imageView: AppCompatImageView = binding.productImage
+        val productTitle: AppCompatTextView = binding.productName
+        val productItemContainer: ConstraintLayout = binding.productItemContainer
+    }
+
+    inner class HeaderViewHolder(binding: FragmentCatalogItemHeaderBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        val headerName: AppCompatTextView = binding.sectionHeader
     }
 
 }
