758bf2d5f38aacb89a1cd66e78dc8002634dd9e1
Ngoni Munyaradzi <ngoni.munyaradzi@gmail.com>
2022-02-22 11:17:02 +0200

    Update data structuring

--- catalogapp/app/src/main/java/com/scribblex/catalogapp/data/repository/CatalogRepository.kt
+++ catalogapp/app/src/main/java/com/scribblex/catalogapp/data/repository/CatalogRepository.kt
@@ -16,16 +16,17 @@ class CatalogRepository @Inject constructor(
     fun fetchCatalog(): Flow<Resource<HashMap<String, MutableList<BaseModel>>>> {
         return remoteDataSource.catalogList
             .transform {
+
                 // TODO: 2022/01/14 Optimize the code below
-                val sectionList: MutableList<BaseModel> = mutableListOf()
                 val groupedMap: HashMap<String, MutableList<BaseModel>> = HashMap()
+                val productList: MutableList<BaseModel> = mutableListOf()
 
                 for (item in it.data!!) {
+
                     val categoryModel = CategoryModel(
                         categoryId = item.id,
                         categoryName = item.name, categoryDescription = item.description
                     )
-                    sectionList.add(categoryModel)
 
                     for (product in item.products) {
                         val productModel = ProductModel(
@@ -33,9 +34,11 @@ class CatalogRepository @Inject constructor(
                             productId = product.id, productName = product.name, url = product.url,
                             productDescription = product.description, salePrice = product.salePrice
                         )
-                        sectionList.add(productModel)
+                        productList.add(productModel)
                     }
-                    groupedMap[item.name] = sectionList
+
+                    groupedMap[categoryModel.categoryName] = productList.toMutableList()
+                    productList.clear()
                 }
                 emit(Resource.success(groupedMap))
             }
